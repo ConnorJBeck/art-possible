bde9f13e5b48627547cb551784f2368d5a87c3a9
Connor Beck <connor.j.beck@gmail.com>
2017-03-11 12:39:51 -0800

    updated positioning

--- src/Picture.js
+++ src/Picture.js
@@ -8,7 +8,7 @@ class Picture extends Component {
   render() {
       const { url, x, y } = this.props;
     return (
-      <img src={url} alt="" style={{position: 'relative', width: '300px'}} />
+      <img src={url} alt="" style={{position: 'relative', height: '350px', paddingRight:'100px' }} />
     );
   }
 }
